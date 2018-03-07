f09f7680742a2bffbd445952a2d8847dae3d2fc4
qianhaibin <Jackson@hboxs.com>
2018-03-07 15:47:00 +0800

    测试了一个rest请求的编写方式

--- src/main/java/cn/qianying/graduaction/controller/VideoController.java
+++ src/main/java/cn/qianying/graduaction/controller/VideoController.java
@@ -1,6 +1,7 @@
 package cn.qianying.graduaction.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,12 +9,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Created by qianhaibin on 2018/3/7.
  */
 @Controller("videoController")
-@RequestMapping(value = "/video")
+@RequestMapping(value = "/video/{username}")
 public class VideoController {
 
-    @RequestMapping(value = "/videoAnalise",method = RequestMethod.GET)
-    public String videoAnalise(){
-        System.out.println("used!!!!!!!!!!!");
+    /**
+     * rest风格请求
+     *
+     * @param id
+     * @param username
+     * @return
+     */
+    @RequestMapping(value = "/videoAnalise/{id}", method = RequestMethod.GET)
+    public String videoAnalise(@PathVariable("id") Integer id, @PathVariable("username") String username) {
+        System.out.println("username: " + username);
+        System.out.println("used!!!!!!!!!!!" + id);
         return "index";
     }
 }
